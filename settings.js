1b63128204cf97c28ee2f9d7746e74b7852fc0af
codexxx16 <codexxxnull@gmail.com>
2025-08-01 11:28:27 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "", // "SUBZERO~a903e25154130bc24cd2eb5de30d8e9c9daccdb5"
+  OWNER_NUMBER: "", /+27642701608/ Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
