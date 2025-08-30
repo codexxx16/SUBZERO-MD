8843f52c3dbfeccde255cde8b0bd548c60e9967b
codexxx16 <codexxxnull@gmail.com>
2025-08-30 12:31:14 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;bAFU0IjA#R_rTxkxQKXJqpYbAPmy0vMVyhvOCzLjZLYWethdwR64", // "*"
+  OWNER_NUMBER: "27742820156", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
 
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
+// CREATED BY X
